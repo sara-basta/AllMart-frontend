fb63dafcd2bca44407ce2d6602647b8cfd6912df
sara8329 <sarabst77@gmail.com>
2026-03-22 14:28:23 +0100

    fix: add real Vercel URL for Stripe redirects

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiUrl: 'https://allmart-backend-b364.onrender.com/api',
   stripePublicKey: 'pk_test_51TBxcgRsei3heG1XzZn2qNGgPWfoqFLI0aTmUil9rdoMTP0PMSd32lM0f8C5jwnzjRYuzxAFsSRqzstXyNCS9Mx00085FMEC1U', 
-  frontendUrl: ''
+  frontendUrl: 'https://allmart-frontend.vercel.app'
 };
