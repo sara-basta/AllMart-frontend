8297126ade68a3124ae0bfb64a36ebc770c4575c
sara8329 <sarabst77@gmail.com>
2026-03-22 14:14:50 +0100

    chore: connect production environment to live Render backend

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: '',
+  apiUrl: 'https://allmart-backend-b364.onrender.com/api',
   stripePublicKey: 'pk_test_51TBxcgRsei3heG1XzZn2qNGgPWfoqFLI0aTmUil9rdoMTP0PMSd32lM0f8C5jwnzjRYuzxAFsSRqzstXyNCS9Mx00085FMEC1U', 
   frontendUrl: ''
 };
